8e4507e8babab013f8047a75370ae56887d16a3b
brener-fregulia <brener.renan.df@gmail.com>
2026-05-08 08:35:03 -0300

    refactor: agrupa ações pós-inventário em post_inventory_maintenance

--- agent/lib/forge-ls.sh
+++ agent/lib/forge-ls.sh
@@ -3,5 +3,14 @@
 # Uso: forge-ls.sh /tmp/mnt_sda3/Users
 DIR="$1"
 [ -z "$DIR" ] && exit 1
+
+# Permite apenas paths dentro de /tmp/mnt_*
+case "$DIR" in
+    /tmp/mnt_*/*)  ;;
+    /tmp/mnt_*/)   ;;
+    /tmp/mnt_*)    ;;
+    *) echo '{"error":"path nao permitido"}' && exit 1 ;;
+esac
+
 timeout 5 find "$DIR" -maxdepth 1 -mindepth 1 -type d 2>/dev/null | awk -F'/' '{print "d\t"$NF}'
 timeout 5 find "$DIR" -maxdepth 1 -mindepth 1 -type f 2>/dev/null | awk -F'/' '{print "f\t"$NF}'
